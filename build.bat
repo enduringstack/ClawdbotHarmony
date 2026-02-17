65eb76137ed1476ef8e47c69574bcc109943b746
Hongjie Liu <liuhongjie1026@gmail.com>
2026-02-16 17:52:36 -0800

    feat: enable voiceprint stub module, fix about page routing (v2.18.15)

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+set DEVECO_SDK_HOME=C:\Program Files\Huawei\DevEco Studio\sdk
+cd /d C:\Users\Liuho\ClawdbotHarmony
+"C:\Program Files\Huawei\DevEco Studio\tools\hvigor\bin\hvigorw.bat" assembleHap --mode module -p product=default -p buildMode=release --no-daemon
