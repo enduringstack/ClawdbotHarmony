da2eb7ea1d01032c147ebbe5c5428516ece69d85
Hongjie Liu <liuhongjie1026@gmail.com>
2026-02-17 08:28:29 -0800

    refactor: 移动所有构建/部署脚本到 scripts 目录

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set DEVECO_SDK_HOME=C:\Program Files\Huawei\DevEco Studio\sdk
-cd /d C:\Users\Liuho\ClawdbotHarmony
-call "C:\Program Files\Huawei\DevEco Studio\tools\hvigor\bin\hvigorw.bat" assembleHap --mode module -p product=default -p buildMode=release --no-daemon
